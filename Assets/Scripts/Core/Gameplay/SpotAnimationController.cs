3e4395563caa69c67455b2b4ced6eb4f27210e04
Ben Tang <78770681+woderggininder@users.noreply.github.com>
2022-01-14 21:32:04 +0800

    Added SpotController.cs

---
+++ Assets/Scripts/Core/Gameplay/SpotAnimationController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Minesweeper.Core
+{
+    public class SpotAnimationController : MonoBehaviour
+    {
+        [SerializeField] private SpotController _spotController;
+    }
+}
