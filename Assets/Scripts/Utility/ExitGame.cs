3985da1847e1f7cf152a603e8bf8449c81089570
Ben Tang <78770681+woderggininder@users.noreply.github.com>
2022-01-15 16:59:04 +0800

    Temporary implementation of Application.Quit()

---
+++ Assets/Scripts/Utility/ExitGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Minesweeper.UI
+{
+    public class ExitGame : MonoBehaviour
+    {
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
