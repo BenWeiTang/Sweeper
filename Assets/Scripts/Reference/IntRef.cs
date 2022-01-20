7790e39392327274ed3fea3e6d92742a439e5108
Ben Tang <78770681+woderggininder@users.noreply.github.com>
2022-01-20 14:54:18 +0800

    Added Int ref

---
+++ Assets/Scripts/Reference/IntRef.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Minesweeper.Reference
+{
+    [CreateAssetMenu(fileName = "New Int Reference", menuName = "3D Minesweeper/Reference/Int")]
+    public class IntRef : ATypeRef<int> { }
+}
