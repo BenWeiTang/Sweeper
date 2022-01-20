752fa274399b443f7503f7a83c35d2448dde66e7
Ben Tang <78770681+woderggininder@users.noreply.github.com>
2022-01-20 15:51:03 +0800

    Added click count SO to PlayerInput.cs

--- Assets/Scripts/Core/Gameplay/PlayerInput.cs
+++ Assets/Scripts/Core/Gameplay/PlayerInput.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using Minesweeper.Reference;
 
 namespace Minesweeper.Core
 {
     public class PlayerInput : MonoBehaviour
     {
         [SerializeField] private Camera _camera;
+        [SerializeField] private IntRef _clickCount;
 
         private ISpot _currentISpot;
         private float _lastEscDown;
+
+        public void OnFirstSafeSpotDug() => _clickCount.value = 1;
 
         private void Update()
         {
@@ -19,42 +23,69 @@ namespace Minesweeper.Core
 
         private void CheckMouseInput()
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && Input.GetMouseButtonDown(1))
+            {
+                DoubleDown(true);
+            }
+            else if (Input.GetMouseButtonDown(0))
             {
                 LeftClick();
 
                 if (Input.GetMouseButton(1))
                 {
-                    DoubleDown();
+                    DoubleDown(false);
                 }
             }
-            if (Input.GetMouseButtonDown(1))
+            else if (Input.GetMouseButtonDown(1))
             {
                 RightClick();
 
                 if (Input.GetMouseButton(0))
                 {
-                    DoubleDown();
+                    DoubleDown(false);
                 }
             }
         }
 
+        // Increment of _click.value has to come before calling _currentISpot.Dig()
+        // If Dig() is called before _click.value++, what will happen is that
+        // OnFirstSafeSpotDug gets called first, which sets the value to 1
+        // and then _clickCount.value gets incremented one more time,
+        // resulting in that the first click will be counted as two
         private void LeftClick()
         {
             UpdateCurrentISpot();
-            _currentISpot?.Dig();
+            if (_currentISpot != null)
+            {
+                _clickCount.value++;
+                _currentISpot.Dig();
+            }
         }
 
         private void RightClick()
         {
             UpdateCurrentISpot();
-            _currentISpot?.Mark();
+            if (_currentISpot != null)
+            {
+                _clickCount.value++;
+                _currentISpot.Mark();
+            }
         }
 
-        private void DoubleDown()
+        // addOne is true when both mouse buttons are pressed during the same frame
+        // In such cases, we want to increment click count by one
+        // addOne is false when one mouse button is down while the other is already on hold
+        // In such cases, we want to decrement click count by one,
+        // as there will be an additional call of RightClick() or LeftClick(),
+        // either of which increments click count by one, which we don't want
+        private void DoubleDown(bool addOne)
         {
             UpdateCurrentISpot();
-            _currentISpot?.ClearNear();
+            if (_currentISpot != null)
+            {
+                _currentISpot.ClearNear();
+                _clickCount.value += addOne ? 1 : -1;
+            }
         }
 
         private void UpdateCurrentISpot()
