0670b5534bcab31d62f170fb85b89ca8be65acda
Ben Tang <78770681+woderggininder@users.noreply.github.com>
2022-01-22 21:18:21 +0800

    Camera automatically moves to start position when coming back from gameplay

--- Assets/Scripts/Animation/Camera/StartSceneCameraController.cs
+++ Assets/Scripts/Animation/Camera/StartSceneCameraController.cs
@@ -6,11 +6,30 @@ namespace Minesweeper.Animation
     public class StartSceneCameraController : MonoBehaviour
     {
         [SerializeField] private Transform _camera;
+
+        [Header("Waypoints")]
         [SerializeField] private Transform _origin;
         [SerializeField] private Transform _start;
         [SerializeField] private Transform _settings;
 
+        [Header("Animation")]
         [SerializeField] private MoveTo _moveToAnim;
+
+        [Header("Type Reference")]
+        [SerializeField] BoolRef _firstEnterSeesion;
+
+        private void Start()
+        {
+            if (_firstEnterSeesion.value)
+            {
+                _firstEnterSeesion.value = false;
+                transform.position = _origin.position;
+            }
+            else
+            {
+                transform.position = _start.position;
+            }
+        }
 
         public async void OnAnyButtonClicked()
         {
@@ -25,6 +44,11 @@ namespace Minesweeper.Animation
         public async void OnBackToStartClicked()
         {
             await _moveToAnim.PerformAsync(_camera, _start.position);
+        }
+
+        private void OnApplicationQuit()
+        {
+            _firstEnterSeesion.value = true;
         }
     }
 }
