9ecc2f01a66d5644e6b17dfa71b3bed4da721f87
Ben Tang <78770681+woderggininder@users.noreply.github.com>
2022-01-22 21:11:02 +0800

    New reference type

---
+++ Assets/Scripts/Reference/BoolRef.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Minesweeper.Reference
+{
+    [CreateAssetMenu(fileName = "New Boolean Reference", menuName = "3D Minesweeper/Reference/Bool")]
+    public class BoolRef : ATypeRef<bool> { }
+}
---
+++ Assets/Scripts/Reference/Vector3Ref.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Minesweeper.Reference
+{
+    [CreateAssetMenu(fileName = "New Vector3 Reference", menuName = "3D Minesweeper/Reference/Vector3")]
+    public class Vector3Ref : ATypeRef<Vector3> { }
+}
