43f3c454006d858d9f8a9de5df0bf6e54afe9ec1
Ben Tang <78770681+woderggininder@users.noreply.github.com>
2022-02-02 12:41:45 +0800

    Phase 2
    Text bubble animation completed

--- Assets/Scripts/UI/PanelController/StatNumberController.cs
+++ Assets/Scripts/UI/PanelController/StatNumberController.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Minesweeper.Animation;
@@ -7,12 +9,22 @@ namespace Minesweeper.UI.PanelController
     public class StatNumberController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] private CanvasGroup _statNumBlock;
+        [SerializeField] private MoveTo _moveUpAnim;
         [SerializeField] private CanvasGroupFade _fadeInAnim;
         [SerializeField] private CanvasGroupFade _fadeOutAnim;
 
         public async void OnPointerEnter(PointerEventData eventData)
         {
-            await _fadeInAnim.PerformAsync(_statNumBlock);
+            Vector3 og = _statNumBlock.transform.position;
+            _statNumBlock.transform.position -= Vector3.up * 10f; 
+            List<Task> tasks = new List<Task>();
+
+            var fadeOperation = _fadeInAnim.PerformAsync(_statNumBlock);
+            var moveOperation = _moveUpAnim.PerformAsync(_statNumBlock.transform, og);
+            tasks.Add(fadeOperation);
+            tasks.Add(moveOperation);
+
+            await Task.WhenAll(tasks);
         }
 
         public async void OnPointerExit(PointerEventData eventData)
