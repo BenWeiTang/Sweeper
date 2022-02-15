4ded6b9ba6d78427efe427932c1bef3fc9ca42c6
Ben Tang <sjps30503@gmail.com>
2022-02-15 18:53:54 -0500

    Update ToggleGroupController.cs

--- Assets/Scripts/UI/ToggleGroupController.cs
+++ Assets/Scripts/UI/ToggleGroupController.cs
@@ -8,13 +8,13 @@ namespace Minesweeper.UI
 {
     public class ToggleGroupController : MonoBehaviour
     {
-        public event Action<ToggleController> ToggleSelected;
-        public int CurrentID => CurrentToggleController.ID;
-
-        private ToggleController CurrentToggleController { get; set; }
         [SerializeField] private List<ToggleController> _controllers;
 
+        public event Action<ToggleController> ToggleSelected;
+        public int CurrentID => CurrentToggleController.ID;
         public void Select(ToggleController toggleController) => ToggleSelected?.Invoke(toggleController);
+
+        private ToggleController CurrentToggleController { get; set; }
 
         public void UpdateCurrentController(int id)
         {
@@ -35,6 +35,18 @@ namespace Minesweeper.UI
 
         private void Start()
         {
+            LoadFromGameSettings();
+        }
+
+        private void OnSelected(ToggleController toggleController)
+        {
+            CurrentToggleController = toggleController;
+        }
+
+        // Needs to be called after Awake because the Toggle Controllers need to register to the event ToggleSelected
+        // Otherwise, when calling Select() at the end of this method, nothing will happen
+        private void LoadFromGameSettings()
+        {
             var targetID = SettingsSerializer.LoadSettings().GeneralSettingsData.Difficulty switch
             {
                 0 => 0,
@@ -43,13 +55,7 @@ namespace Minesweeper.UI
                 _ => 0,
             };
             CurrentToggleController = _controllers.First(c => c.ID == targetID);
-            Debug.Log(CurrentToggleController.ID);
             Select(CurrentToggleController);
-        }
-
-        private void OnSelected(ToggleController toggleController)
-        {
-            CurrentToggleController = toggleController;
         }
     }
 }
