3897b1957fe9cdb689b3af810a19209c22790cb2
Ben Tang <sjps30503@gmail.com>
2022-02-15 18:50:08 -0500

    Load difficulty from GameSettings.json

--- Assets/Scripts/UI/ToggleGroupController.cs
+++ Assets/Scripts/UI/ToggleGroupController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Minesweeper.Saving;
 using UnityEngine;
 
 namespace Minesweeper.UI
@@ -9,9 +10,9 @@ namespace Minesweeper.UI
     {
         public event Action<ToggleController> ToggleSelected;
         public int CurrentID => CurrentToggleController.ID;
-        
+
         private ToggleController CurrentToggleController { get; set; }
-        private List<ToggleController> _controllers = new List<ToggleController>();
+        [SerializeField] private List<ToggleController> _controllers;
 
         public void Select(ToggleController toggleController) => ToggleSelected?.Invoke(toggleController);
 
@@ -26,13 +27,24 @@ namespace Minesweeper.UI
             var target = _controllers.First(c => c.ID == id);
             target.Select();
         }
-        
+
         private void Awake()
         {
             ToggleSelected += OnSelected;
-            _controllers = GetComponentsInChildren<ToggleController>().ToList();
-            // Grab the one that is manually set to active in the inspector as the current active toggle controller
-            CurrentToggleController = _controllers.First(c => c.IsOn);
+        }
+
+        private void Start()
+        {
+            var targetID = SettingsSerializer.LoadSettings().GeneralSettingsData.Difficulty switch
+            {
+                0 => 0,
+                1 => 1,
+                2 => 2,
+                _ => 0,
+            };
+            CurrentToggleController = _controllers.First(c => c.ID == targetID);
+            Debug.Log(CurrentToggleController.ID);
+            Select(CurrentToggleController);
         }
 
         private void OnSelected(ToggleController toggleController)
