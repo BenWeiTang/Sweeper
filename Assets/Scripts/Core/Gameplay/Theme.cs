0c12d6e61762901c192fed6dfd90b62919c720b1
Ben Tang <sjps30503@gmail.com>
2022-02-27 15:36:58 -0500

    Added SO Theme and created one for default

---
+++ Assets/Scripts/Core/Gameplay/Theme.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Minesweeper.Core
+{
+    [CreateAssetMenu(fileName = "Theme", menuName = "3D Minesweeper/Theme")]
+    public class Theme : ScriptableObject
+    {
+        [Header("Body")]
+        public Color DugBase;
+        public Color DugEdge;
+        public Color MarkedBase;
+        public Color MarkedEdge;
+        public Color MineBase;
+        public Color MineEdge;
+        public Color UntouchedBase;
+        public Color UntouchedEdge;
+
+        [Header("Number")]
+        public Color One;
+        public Color Two;
+        public Color Three;
+        public Color Four;
+        public Color Five;
+        public Color Six;
+        public Color Seven;
+        public Color Eight;
+    }
+}
