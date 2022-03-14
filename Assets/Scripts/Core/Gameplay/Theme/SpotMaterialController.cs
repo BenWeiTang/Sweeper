2bfa0aa9209224806d9e66b45a6c1e2535aba50d
Ben Tang <sjps30503@gmail.com>
2022-03-14 17:21:18 -0400

    Update SpotMaterialController.cs
    Changed to use materials instead of setting the colors of the materials.
    
    Because we use the MeshRenderer.materials function, which returns a copy of the array, we have to call GetComponent to actually change the array itself when updating materials.

--- Assets/Scripts/Core/Gameplay/Theme/SpotMaterialController.cs
+++ Assets/Scripts/Core/Gameplay/Theme/SpotMaterialController.cs
@@ -13,7 +13,7 @@ namespace Minesweeper.Core
         [SerializeField] private GameObject _mineBlock;
 
         private Theme _theme;
-        
+
         private void Awake()
         {
             _theme = SettingsManager.Instance.CurrentTheme;
@@ -25,52 +25,59 @@ namespace Minesweeper.Core
             // At index 0 -> edge material
             // At index 1 -> base material
             // At index 2 -> number material (if applicable)
-            
+
+            // Setting the number blocks, which uses dug mats
             for (int i = 0; i < _numBlocks.Count; i++)
             {
-                var materials = _numBlocks[i].GetComponent<MeshRenderer>().materials;
-                materials[0].color = _theme.DugEdge;
-                materials[1].color = _theme.DugBase;
-
-                switch (i)
+                // We need to assign this entire newly created array to MeshRenderer.materials
+                // When doing so, we have to call the GetComponent function;
+                // Caching a reference to that array has not effect because MeshRenderer.materials
+                // actually returns a copy
+                Material[] newMaterials; 
+                
+                // For 0, there are only two materials because it is just a blank spot
+                if (i == 0)
                 {
-                    case 1:
-                        materials[2].color = _theme.One;
-                        break;
-                    case 2:
-                        materials[2].color = _theme.Two;
-                        break;
-                    case 3:
-                        materials[2].color = _theme.Three;
-                        break;
-                    case 4:
-                        materials[2].color = _theme.Four;
-                        break;
-                    case 5:
-                        materials[2].color = _theme.Five;
-                        break;
-                    case 6:
-                        materials[2].color = _theme.Six;
-                        break;
-                    case 7:
-                        materials[2].color = _theme.Seven;
-                        break;
-                    case 8:
-                        materials[2].color = _theme.Eight;
-                        break;
+                    newMaterials = new[] {_theme.DugEdgeMaterial, _theme.DugBaseMaterial};
+                    _numBlocks[i].GetComponent<MeshRenderer>().materials = newMaterials;
+                    continue;
                 }
+
+                newMaterials = new Material[3];
+                newMaterials[0] = _theme.DugEdgeMaterial;
+                newMaterials[1] = _theme.DugBaseMaterial;
+
+                newMaterials[2] = i switch
+                {
+                    1 => _theme.OneMaterial,
+                    2 => _theme.TwoMaterial,
+                    3 => _theme.ThreeMaterial,
+                    4 => _theme.FourMaterial,
+                    5 => _theme.FiveMaterial,
+                    6 => _theme.SixMaterial,
+                    7 => _theme.SevenMaterial,
+                    8 => _theme.EightMaterial,
+                    _ => newMaterials[2]
+                };
+
+                _numBlocks[i].GetComponent<MeshRenderer>().materials = newMaterials;
             }
 
-            var mineBlockMat = _mineBlock.GetComponent<MeshRenderer>().materials;
-            var untouchedBlockMat = _untouchedBlock.GetComponent<MeshRenderer>().materials;
-            var markedBlockMat = _markedBlock.GetComponent<MeshRenderer>().materials;
+            // Setting special blocks mats
+            var newMineMats = new Material[2];
+            var newUntouchedMats = new Material[2];
+            var newMarkedMats = new Material[2];
+            
+            newMineMats[0] = _theme.MineEdgeMaterial;
+            newMineMats[1] = _theme.MineBaseMaterial;
+            newUntouchedMats[0] = _theme.UntouchedEdgeMaterial;
+            newUntouchedMats[1] = _theme.UntouchedBaseMaterial;
+            newMarkedMats[0] = _theme.MarkedEdgeMaterial;
+            newMarkedMats[1] = _theme.MarkedBaseMaterial;
 
-            mineBlockMat[0].color = _theme.MineEdge;
-            mineBlockMat[1].color = _theme.MineBase;
-            untouchedBlockMat[0].color = _theme.UntouchedEdge;
-            untouchedBlockMat[1].color = _theme.UntouchedBase;
-            markedBlockMat[0].color = _theme.MarkedEdge;
-            markedBlockMat[1].color = _theme.MarkedBase;
+            _mineBlock.GetComponent<MeshRenderer>().materials = newMineMats;
+            _untouchedBlock.GetComponent<MeshRenderer>().materials = newUntouchedMats;
+            _markedBlock.GetComponent<MeshRenderer>().materials = newMarkedMats;
         }
     }
 }
