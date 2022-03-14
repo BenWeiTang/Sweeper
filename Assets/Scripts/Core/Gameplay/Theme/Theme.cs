ce118835f212c29ed157dc2473ebc1e6a156cb1b
Ben Tang <sjps30503@gmail.com>
2022-03-14 16:18:34 -0400

    New theme skeletoned

--- Assets/Scripts/Core/Gameplay/Theme/Theme.cs
+++ Assets/Scripts/Core/Gameplay/Theme/Theme.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Minesweeper.Audio;
 
 namespace Minesweeper.Core
 {
@@ -27,5 +28,31 @@ namespace Minesweeper.Core
 
         [Header("Camera")]
         public Color BackgroundColor;
+
+        [Header("Material")]
+        public Material DugEdgeMaterial;
+        public Material DugBaseMaterial;
+        public Material MarkedEdgeMaterial;
+        public Material MarkedBaseMaterial;
+        public Material MineEdgeMaterial;
+        public Material MineBaseMaterial;
+        public Material UntouchedEdgeMaterial;
+        public Material UntouchedBaseMaterial;
+        public Material OneMaterial;
+        public Material TwoMaterial;
+        public Material ThreeMaterial;
+        public Material FourMaterial;
+        public Material FiveMaterial;
+        public Material SixMaterial;
+        public Material SevenMaterial;
+        public Material EightMaterial;
+
+        [Header("Lighting")]
+        [Tooltip("Tracking light is the light source that follows the position of the mouse. If the materials are unlit, enabling this option will have no effect.")]
+        public bool UseTrackingLight = false;
+
+        [Header("Audio")]
+        public BGMSoundBank BGMSoundBank;
+        public GameplayEffectSoundBank GameplayEffectSoundBank;
     }
 }
