d722231becd6d4b1f7ce6aab4cf96d1ba4ba9c9b
Ben Tang <sjps30503@gmail.com>
2022-05-13 16:13:00 -0400

    Fixed auto volume tuning

--- Assets/Scripts/Extension/AudioExtensions.cs
+++ Assets/Scripts/Extension/AudioExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -5,19 +6,23 @@ namespace Minesweeper.Extensions
 {
     public static class AudioExtensions
     {
+        private const float InverseE = 1 / (float) Math.E;
+        
         /// <summary>
-        /// 
+        /// Change the volume of this Audio Mixer.
         /// </summary>
         /// <param name="mixer"></param>
         /// <param name="exposedName">The name of 'The Exposed to Script' variable</param>
         /// <param name="value">value must be between 0 and 1</param>
         public static void SetVolume(this AudioMixer mixer, string exposedName, float value)
         {
-            mixer.SetFloat(exposedName, Mathf.Lerp(-80.0f, 0.0f, Mathf.Clamp01(value)));
+            value = Mathf.Clamp01(value);
+            value = Mathf.Pow(value, InverseE);
+            mixer.SetFloat(exposedName, Mathf.Lerp(-80.0f, 0.0f, value));
         }
  
         /// <summary>
-        /// 
+        /// Get the volume of this Audio Mixer.
         /// </summary>
         /// <param name="mixer"></param>
         /// <param name="exposedName">The name of 'The Exposed to Script' variable</param>
@@ -26,7 +31,9 @@ namespace Minesweeper.Extensions
         {
             if (mixer.GetFloat(exposedName, out float volume))
             {
-                return Mathf.InverseLerp(-80.0f, 0.0f, volume);
+                float result = Mathf.InverseLerp(-80.0f, 0.0f, volume);
+                result = Mathf.Pow(result, (float) Math.E);
+                return result;
             }
  
             return 0f;
